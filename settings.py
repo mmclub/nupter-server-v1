631ddc2da4bf51d736c9c7b42903c26863126b62
Linxiangyu <lxyweb@gmail.com>
2013-08-14 15:52:09 +0800

    Config For Heroku

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 # Django settings for nuptsite project.
 
 import os.path
+import dj_database_url
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,16 +11,21 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+
+
+BASE_DIR = '/Users/linxiangyu/django/nuptsite/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/linxiangyu/django/nuptsite/nuptsite/student.db',                      # Or path to database file if using sqlite3.
+        'NAME': BASE_DIR + 'student.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+DATABASES['default'] =  dj_database_url.config()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
